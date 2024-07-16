8d0d910014cca695fc564f152a0ebe0afda2a0a5
Arayor <aaron.salasrodriguez@gmail.com>
2024-07-15 21:08:08 -0400

    Added slug support

--- src/store/proveedor/thunks.js
+++ src/store/proveedor/thunks.js
@@ -1,23 +1,29 @@
 import { providerApi } from "./helpers";
-import { provider_getall_failure, provider_getall_request, provider_getall_success, provider_set } from "./proveedorSlice";
+import {
+  provider_getall_failure,
+  provider_getall_request,
+  provider_getall_success,
+  provider_set,
+} from "./proveedorSlice";
 
-export const startListProveedores = () => {
+export const startListProveedores = (slug) => {
   return async (dispatch) => {
-    dispatch( provider_getall_request() );
+    dispatch(provider_getall_request());
 
-    const {data} = await providerApi();
-		let error = data.error;
-    if( error  ) {
-      dispatch( provider_getall_failure(error.toString()) );
+    const { data } = await providerApi();
+    let error = data.error;
+    if (error) {
+      dispatch(provider_getall_failure(error.toString()));
       return;
     }
 
-    dispatch( provider_getall_success(data) )
+    dispatch(provider_getall_success(data));
 
-
-    const idProveedor = process.env.REACT_APP_ID_PROVIDER_EXAMPLE;
-    const myProvider=data.filter((e) => e._id === idProveedor)[0]
-    dispatch( provider_set(myProvider));
-
+    const myProvider = data.filter((e) => e.slugUrl === slug)[0];
+    if (!myProvider) {
+      dispatch(provider_getall_failure("Sin proveedor seleccionado"));
+      return;
+    }
+    dispatch(provider_set(myProvider));
   };
 };
