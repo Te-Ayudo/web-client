6179829c297f0d59453ff6b4c99ef8308d499815
Ilse Romero <ilseromero35@gmail.com>
2025-03-20 08:23:56 -0400

    fix(booking): change employee endpoint.

--- src/store/booking/helpers/employeeApi.js
+++ src/store/booking/helpers/employeeApi.js
@@ -1,33 +1,39 @@
 import _fetch from "../../../wrappers/_fetch";
 
-export const employeeApi = async(method,service,id_prov) => {
+export const employeeApi = async (method, service, id_prov) => {
+  const baseUrl = process.env.REACT_APP_API_URL + `/employee/provider/${id_prov}`;
 
-  // const idProveedor = process.env.REACT_APP_ID_PROVIDER_EXAMPLE;
-  const teayudoUrl= process.env.REACT_APP_API_URL+'/employee/provider/'+id_prov;
+  // Construcción de parámetros de la URL
+  const queryParams = new URLSearchParams({
+    method: method,
+    page: 1,
+    perPage: 25,
+    sort: "-id",
+    state: "true",
+  });
+
+  // Agregar los servicios como serviceCart.0, serviceCart.1, etc.
+  service.forEach((srv, index) => {
+    queryParams.append(`serviceCart.${index}`, srv);
+  });
+
+  const fullUrl = `${baseUrl}?${queryParams.toString()}`;
 
   try {
+    const resp = await _fetch(fullUrl, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-    const resp = await _fetch( teayudoUrl, {
-					method: 'POST',
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
-						method: method,
-						serviceCart: service
-					}),
-				});
-
-    if(!resp.ok) throw new Error('No se pudo obtener la lista de empleados');
+    if (!resp.ok) throw new Error("No se pudo obtener la lista de empleados");
 
     const myResp = await resp.json();
     return myResp;
-
   } catch (error) {
     console.log(error);
-    throw new Error( error.message );
+    throw new Error(error.message);
   }
-
-
-}
+};
