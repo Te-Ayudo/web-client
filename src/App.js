860f7bf3cd33431356874125242bf1dc7459dff5
miromero13 <ilseromero35@gmail.com>
2025-06-22 17:59:59 -0400

    fix: Update route path to include bookingId parameter for Gracias page

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
             } 
           />
           <Route 
-            path=":providerid/gracias" 
+            path=":providerid/gracias/:bookingId" 
             element={
               <RutaProtegida>
                 <Thanks />
